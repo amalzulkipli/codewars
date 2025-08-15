3685a766eb127603b5f1580b4806940f49241a94
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-08-15 08:25:57 +0000

    15-august-2025

---
+++ Python/detect_pangram.py
@@ -0,0 +1,7 @@
+def is_pangram(st):
+    lowercased = st.lower() 
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        if letter not in lowercased:
+            return False
+    return True
+        
