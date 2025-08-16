a1c38917603391a967024dbeb13df20e3df66d93
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-08-16 03:27:40 +0000

    16-august-2025

---
+++ Python/find_maximum_and_minimum_values_of_a_list.py
@@ -0,0 +1,15 @@
+# my solution
+def minimum(arr):
+    mn = min(arr)
+    return mn
+
+def maximum(arr):
+    mx = max(arr)
+    return mx
+
+# better pythonic solution
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
