2ecd627dd70af45d13b3353642de0996e60d1874
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-08-21 04:42:56 +0000

    21-august-2025

---
+++ Python/build_a_pile_of_cubes.py
@@ -0,0 +1,27 @@
+# my solution -- which is wrong miserably, forcing quadratic equation on sum of cubes
+import math
+
+def find_nb(m):
+    m = (m ** 0.5) * 2
+    
+    a = 1 
+    b = 1 
+    c = -m
+    
+    d = 1 - (4 * 1 * c) 
+    
+    ans2 = (-b + math.sqrt(d))/(2 * a)
+    
+    return ans2 if ans2.is_integer() else -1
+
+# better pythonic
+import math
+
+def find_nb(m):
+    n = int(math.isqrt(2 * math.isqrt(m)))  
+    
+    while (n * (n + 1) // 2) ** 2 < m:
+        n += 1
+        
+    return n if (n * (n + 1) // 2) ** 2 == m else -1
+
