b5b866f7e8fa2be04d42dc5e76a5200e80b200a3
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-08-21 04:45:02 +0000

    21-august-2025

--- Python/build_a_pile_of_cubes.py
+++ Python/build_a_pile_of_cubes.py
@@ -25,3 +25,14 @@ def find_nb(m):
         
     return n if (n * (n + 1) // 2) ** 2 == m else -1
 
+# and this -- other solution, humbled me down...
+def find_nb(m):
+    n = 1
+    volume = 0
+    while volume < m:
+        volume += n**3
+        if volume == m:
+            return n
+        n += 1
+    return -1
+
