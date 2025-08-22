d8146892eaf18379680c8a1055f89f7bbd348b5d
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-08-22 10:56:53 +0800

    thinkful_logic_drills_traffic_light.py

---
+++ Python/x.py
@@ -0,0 +1,12 @@
+# my solution
+def update_light(current):
+    if current == 'green':
+        return 'yellow'
+    elif current == 'yellow':
+        return 'red'
+    else:
+        return 'green'
+
+# pythonic
+def update_light(current):
+    return {"green": "yellow", "yellow": "red", "red": "green"}[current]
