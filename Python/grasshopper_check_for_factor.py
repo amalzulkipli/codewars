b89b9793d95f4dcea83ceb1d88af6ee41c95d125
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-08-25 09:59:27 +0800

    25-august-2025

---
+++ Python/grasshopper_check_for_factor.py
@@ -0,0 +1,7 @@
+# my solution
+def check_for_factor(base, factor):
+    return True if base % factor == 0 else False
+
+# pythonic lol
+def check_for_factor(base, factor):
+    return base % factor == 0
