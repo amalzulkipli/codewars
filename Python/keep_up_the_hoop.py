782a9934b3d740da2c1e541e0fbd1f57c8ae618a
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-09-03 21:52:25 +0800

    3-september-2025

---
+++ Python/keep_up_the_hoop.py
@@ -0,0 +1,10 @@
+# my sol
+def hoop_count(n):
+    if n < 10:
+        return "Keep at it until you get it"
+    else:
+        return "Great, now move on to tricks"
+
+# pythonic
+def hoopCount(n):
+    return "Keep at it until you get it" if n<10 else "Great, now move on to tricks"
