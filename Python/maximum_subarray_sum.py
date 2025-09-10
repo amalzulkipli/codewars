1d8afc74650ccda8ec33f38a99f78c554134d968
amalzul <119765121+amalzulkipli@users.noreply.github.com>
2025-09-10 16:19:36 +0800

    10-september-2025

---
+++ Python/maximum_subarray_sum.py
@@ -0,0 +1,11 @@
+# mine 
+def max_sequence(arr):
+    max_sum = 0
+    current_sum = 0
+    
+    for num in arr:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+    
+    return max_sum
+
